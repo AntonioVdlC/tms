012d79427b97ce38c5fc7b1afd257848d5649d08
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2021-03-24 23:23:20 +0100

    Add more dev tools (modern FE dev is great)

--- www/src/components/HelloWorld.vue
+++ www/src/components/HelloWorld.vue
@@ -1,30 +1,40 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
+    <h1>{{ msg }}</h1>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
+    <p>
+      <a class="text-green-500 " href="https://vitejs.dev/guide/features.html" target="_blank">
+        Vite Documentation
+      </a>
+      |
+      <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    </p>
 
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <button @click="increment">count is: {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
+  </div>
 </template>
 
-<script setup>
-import { defineProps, reactive } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const state = reactive({ count: 0 })
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+};
 </script>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
