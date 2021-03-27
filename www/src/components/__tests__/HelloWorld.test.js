046d71f93b8e6833be33ec0498bb158b2af3eaad
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2021-03-27 01:26:03 +0100

    Fix tests

--- www/src/components/__tests__/HelloWorld.test.js
+++ www/src/components/__tests__/HelloWorld.test.js
@@ -4,8 +4,22 @@ import HelloWorld from "../HelloWorld.vue";
 describe("HelloWorld.vue", () => {
   test("renders props.msg when passed", () => {
     const msg = "new message";
+
+    const $store = {
+      getters: {
+        "counter/count": function () {
+          return 0;
+        },
+      },
+    };
+
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
+      global: {
+        mocks: {
+          $store,
+        },
+      },
     });
 
     expect(wrapper.get("[data-test=title]").text()).toMatch(msg);
