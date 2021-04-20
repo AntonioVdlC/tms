2cefd5a791d2a8017c8614b0dd573c7cba07feed
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2021-04-20 21:37:11 +0200

    Radio Check. Over. Roger, server. Client reads you loud and clear.

--- www/vite.config.js
+++ www/vite.config.js
@@ -1,16 +1,28 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: [
+        {
+          find: "@",
+          replacement: path.resolve(__dirname, "./src"),
+        },
+      ],
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_SERVER_PROXY,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
-    ],
-  },
+    },
+  };
 });
