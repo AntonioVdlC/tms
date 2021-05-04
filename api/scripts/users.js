276f3744bef368cfb493be8468274d8c5f52c192
Akilesh Sethu <asethu19@gmail.com>
2021-05-05 00:14:48 +0800

    removing unncessary indexes

--- api/scripts/users.js
+++ api/scripts/users.js
@@ -1,4 +1,2 @@
 db.createCollection('users')
 db.users.createIndex({"email": 1}, {unique: true})
-db.users.createIndex({"email": 1, "is_deleted": 1})
-db.users.createIndex({"_id": 1, "is_deleted": 1})
